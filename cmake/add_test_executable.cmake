d8566e14186b96b14e6b446aecbe588217b8954c
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-06-13 22:06:55 +0200

    exclude test executables from all target

--- cmake/add_test_executable.cmake
+++ cmake/add_test_executable.cmake
@@ -1,5 +1,6 @@
 function(add_test_executable name)
     add_executable(${ARGV})
+    set_target_properties("${name}" EXCLUDE_FROM_ALL ON)
     target_link_libraries("${name}"
             GTest::gtest_main
     )
